8de9b36e6e672fa00a9d41f2dbd32dbdc8dab0cc
mksiq <msiqueira@myseneca.ca>
2020-12-31 19:23:15 -0500

    added condition to show nav links to logged or not user

--- controller/general.js
+++ controller/general.js
@@ -3,9 +3,11 @@ import express from 'express';
 const router = new express.Router();
 
 router.get('/', (req, res)=>{
-  res.render('general/index');
+  const user = req.session.user;
+  res.render('general/index', {
+    user: user,
+  });
 });
-
 
 
 export default router;
